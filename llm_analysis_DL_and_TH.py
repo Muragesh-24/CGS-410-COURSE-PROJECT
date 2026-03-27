a843d807c0b60c1abd5ae279e2aad88e0f3dd5db
Muragesh-24 <nyamagoudmurgesh@gmail.com>
2026-03-27 22:55:06 +0530

    setup : base setup done for the llm analysis

---
+++ llm_analysis_DL_and_TH.py
@@ -0,0 +1,8 @@
+# using our data for prediction
+# making two types of sentence for prediction
+# in prediction make use of other parameters too
+# llm text dependence test
+# What LLM does similar to humans
+# What LLM does differently
+# Why transformers might show DLM
+# Limitations of study
